a62852d3a22fe7a67f15868c3d769091e7bca674
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2024-06-22 23:09:23 +0100

    cleanup

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ async function playSound({channel, pitch, duration, volume}) {
       oscillator.type = 'sine'; // Default waveform type
       // pitch is in quarter-semitones from middle C
       const multiplier = Math.pow(2, (pitch - 53) / 48);
+      console.log(pitch)
+      console.log(multiplier)
       oscillator.frequency.value = 256 * multiplier;
       console.log(oscillator.frequency.value)
       gainNode.gain.value = volume / 15; // BBC Micro volume is 0-15, normalize to 0-1
@@ -19,7 +21,7 @@ async function playSound({channel, pitch, duration, volume}) {
       gainNode.connect(audioContext.destination);
 
       oscillator.start();
-      oscillator.stop(audioContext.currentTime + duration / 100); // Duration in 1/100th seconds
+      oscillator.stop(audioContext.currentTime + duration / 20); // Duration in 1/20th seconds
       oscillator.onended = resolve;
     }
   )
@@ -48,37 +50,24 @@ async function playEnvelope(number, attack, decay, sustain, release, peak, susta
   oscillator.onended = resolve;
 }
 
-// // Event listeners for buttons
-// document.getElementById('playSound').addEventListener('click', () => {
-//   playSound({channel: 0, pitch: 440, duration: 100, volume: 15}); // Example SOUND: channel 0, pitch 440Hz, duration 1s, max volume
-// });
-//
-// document.getElementById('playEnvelope').addEventListener('click', () => {
-//   playEnvelope(0, 100, 100, 200, 100, 15, 10, 440, 0, 0); // Example ENVELOPE
-// });
-//
 document.getElementById("playSound").addEventListener('click', async () => {
+  const pitchShift = parseInt(document.getElementById("pitchShift").value);
+  const baseDuration = parseInt(document.getElementById("baseDuration").value);
+
   const commands = document.getElementById("commands").value.split('\n');
   for (let i = 0; i < commands.length; i++) {
     const pitches = commands[i].split(',');
-    const promises = pitches.map((pitch, i) => playSound(
-      {
-        channel: i, pitch: parseInt(pitch.trim()) - 60, duration: 50, volume: 15 / Math.pow(3, i)
-      })
+    await Promise.all(
+      pitches.map((pitch, i) => {
+          return playSound(
+            {
+              channel: i,
+              pitch: parseInt(pitch.trim()) + pitchShift,
+              duration: baseDuration,
+              volume: 15 / Math.pow(4, i)
+            });
+        }
+      )
     )
-    // await all the promises
-    await Promise.all(promises);
-    /*
-    if (command.length === 4) {
-      await playSound({
-        channel: command[0],
-        pitch: command[1],
-        duration: command[2],
-        volume: command[3]
-      });
-    } else if (command.length === 6) {
-      await playEnvelope(command[0], command[1], command[2], command[3], command[4], command[5]);
-    }
-     */
   }
 })
